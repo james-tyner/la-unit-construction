0b8e763948d89f9e760ed09ce12ccc87321a7fe7
James Tyner <services@jamestyner.com>
2019-02-25 17:52:23 -0800

    added cron job

--- runEachWeek.py
+++ runEachWeek.py
@@ -2,4 +2,4 @@ import lib.createTableProjects # drop the projects table and recreate it
 import lib.obtainData # fill in the projects table
 import lib.calculateYearlyUnits # update the number of units per year
 
-import runSendEmail
+# import runSendEmail
