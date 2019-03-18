fbc0a2be9167b73455a63112f7c5752c92530391
James Tyner <services@jamestyner.com>
2019-03-18 15:41:17 -0700

    Added search with autocomplete

--- public/js/info-scripts.js
+++ public/js/info-scripts.js
@@ -1,5 +1,11 @@
 const requestedZip = new URLSearchParams(window.location.search).get('zip');
 
+if (!requestedZip){
+  window.location.replace("https://additup.jamestyner.com");
+}
+
+document.title = `${requestedZip} | Add it up.`;
+
 function getZIPCodeInfo() {
   return axios.get(`/api/neighborhoods/${requestedZip}`);
 }
@@ -7,6 +13,25 @@ function getZIPCodeInfo() {
 function getZIPCodeProjects() {
   return axios.get(`/api/projects/${requestedZip}`);
 }
+
+axios.get("/api/neighborhoods").then(function(response){
+  let neighborhoods = []
+  for (var i=0; i < response.data.length; i++){
+    neighborhoods.push({
+      "label":(response.data[i].zipCode + " - " + response.data[i].description).toString(),
+      "value":response.data[i].zipCode
+    });
+  }
+
+  $('#search input').autocomplete({
+    source: neighborhoods,
+    minLength: 2,
+    select: function(event, ui) {
+      $("#search input").val(ui.item.value);
+      $("#search").submit();
+    }
+  });
+});
 
 axios.all([getZIPCodeInfo(), getZIPCodeProjects()]).then(axios.spread(function(information, projects){
 
--- public/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var neighborhoods = [
-
-  ];
-
-  $("#search").autocomplete({
-    source:neighborhoods
-  });
-});
