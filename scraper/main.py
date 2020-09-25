e4762a631f4610a056688a0cd7aaa84fc8143bfd
James Tyner <services@jamestyner.com>
2020-09-24 20:49:16 -0700

    modified the code to satisfy Google Cloud

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,6 @@
-def getAndStore():
+def get_and_store(event, context):
+    # added event and context to satisfy Pub/Sub & Cloud Functions… not doing anything with them
+
     import components.obtainData # fill in the projects dataset
     import components.createGeoJSON # create GeoJSON files for each ZIP code
     import components.calculateYearlyUnits # update the number of units per year
