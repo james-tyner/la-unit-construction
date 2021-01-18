aafe2ac62fbc40b1419a051d2e3249f05f8fbce6
James Tyner <services@jamestyner.com>
2021-01-18 13:39:31 -0800

    moved the homepage map back into the main page to use asyncData

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -24,7 +24,9 @@
     <main>
       <h2>Find your neighborhood</h2>
 
-      <homepage-map></homepage-map>
+      <figure id="map-container">
+        <div id="map"></div>
+      </figure>
 
       <div id="zip-navigator-list">
         <nuxt-link :to="'/info/' + zip" :key="zip" v-for="zip in Object.keys(zipData)" class="zip-link-row">
@@ -37,12 +39,12 @@
 </template>
 
 <script>
-import HomepageMap from "~/components/HomepageMap.vue";
+import * as turf from '@turf/turf';
+
 import HomepageChart from "~/components/HomepageChart.vue";
 
 export default {
   components:{
-    HomepageMap,
     HomepageChart
   },
   async asyncData(){
@@ -50,14 +52,31 @@ export default {
 
     const zipData = await fetch(`${process.env.apiUrl}/api/neighborhoods`).then(res => res.json())
 
+    const zipBoundaries = await fetch(`${process.env.apiUrl}/api/boundary/zips`).then(res => res.json());
+
+    const cityBoundary = await fetch(`${process.env.apiUrl}/api/boundary/city`).then(res => res.json());
+
     return {
       zipData,
-      summary
+      summary,
+      zipBoundaries,
+      cityBoundary
     }
   },
   head(){
     return {
-      title:"Add it up."
+      title:"Add it up.",
+      link:[
+        {
+            href:'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css',
+            rel:'stylesheet'
+        }
+      ],
+      script:[
+        {
+            src:'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'
+        }
+      ]  
     }
   },
   methods:{
@@ -67,7 +86,125 @@ export default {
       }
       return number;
     }
-  }
+  },
+  mounted(){
+    const mapboxgl = require('mapbox-gl');  
+
+    const map = new mapboxgl.Map({
+        accessToken:process.env.mapboxApiKey,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center:[-118.2448473,34.053718],
+        zoom:9
+    })
+
+    map.on("load", () => {
+        // Add all data sources
+        map.addSource("boundaries", {
+        "type":"geojson",
+        "data":this.zipBoundaries,
+        "generateId":true
+        })
+
+        map.addSource("city-boundary", {
+        "type":"geojson",
+        "data":this.cityBoundary
+        })
+
+        // Add all layers based on data sources
+        map.addLayer({
+        "id":"city-boundary",
+        "type":"line",
+        "source":"city-boundary",
+        "paint":{
+            "line-color":"#333333",
+            "line-width":3
+        }
+        })
+
+        map.addLayer({
+        "id":"boundaries",
+        "type":"fill",
+        "source":"boundaries",
+        "paint":{ 
+            "fill-color":"#754aed",
+            "fill-opacity":[
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            1,
+            0.5
+            ]
+        }
+        })
+    })
+
+    // create the popup that will go over the ZIP when hovered
+    let popup = new mapboxgl.Popup({
+        closeButton:false,
+        closeOnClick:false
+    })
+
+    let popupTemplate = (zip) => {
+        return `<h5 class='map-zip-code'>${zip}</h5>`
+    }
+
+    // create the hover effect with the popup
+    let zipArea = null;
+
+    map.on("mousemove", "boundaries", event => {
+        map.getCanvas().style.cursor = "pointer";
+
+        if (zipArea){
+        map.removeFeatureState({
+            source:"boundaries",
+            id:zipArea
+        })
+        }
+
+        if (event.features.length > 0){
+        //change the opacity
+        zipArea = event.features[0].id;
+
+        map.setFeatureState({
+            source:"boundaries",
+            id:zipArea
+        }, {
+            hover: true
+        })
+
+        // add the popup
+        let coordinates = event.features[0].geometry;
+        let centerPoint = turf.center(coordinates);
+        let center = turf.getCoords(centerPoint)
+
+
+        let zipCode = event.features[0].properties.zipcode;
+
+        popup.setLngLat([center[0], center[1]]).setHTML(popupTemplate(zipCode)).addTo(map);
+        }
+    })
+
+    map.on("mouseleave", "boundaries", event => {
+        // accommodate mouse leave
+        if (zipArea){
+        map.setFeatureState({
+            source:"boundaries",
+            id:zipArea
+        }, {
+            hover: false
+        })
+        }
+
+        map.getCanvas().style.cursor = "";
+        popup.remove();
+    })
+
+    map.on("click", "boundaries", event => {
+        let zipCode = event.features[0].properties.zipcode;
+
+        this.$router.push(`/info/${zipCode}`);
+    })
+    }
 }
 </script>
 
